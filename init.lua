561b599cd1464f573eec0caa41dcf4dc26881bd9
365ffffff <249869974@qq.com>
2019-04-22 22:27:35 +0800

    clean the code

--- init.lua
+++ init.lua
@@ -1,12 +1,5 @@
 print("init lua is runed")
 
 
-gpio.mode(2, gpio.INPUT);
-gpio.mode(0, gpio.OUTPUT);  
-gpio.mode(1, gpio.OUTPUT);
-gpio.write(0,gpio.HIGH);  
-gpio.write(1,gpio.HIGH);
-
-
 
 dofile("webserver.lua")
